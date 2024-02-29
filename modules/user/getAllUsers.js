438b8f798b0cfd1905162d5bdbf1906449d624c9
qedemon <qedemon@snu.ac.kr>
2024-02-29 01:21:35 +0000

    정렬순서 변경

--- modules/user/getAllUsers.js
+++ modules/user/getAllUsers.js
@@ -4,7 +4,10 @@ const {User} = require("models");
 async function getAllUsers(filter, select){
     try{
         await connect();
-        const users = await User.find(filter).select(select).sort({createdAt: -1, aaaNo: -1});
+        const users = (await User.find(filter).select(select)).sort(({aaaNo: A}, {aaaNo: B})=>{
+            const extract = (aaaNo)=>parseInt(aaaNo.match(/\d+$/)[0]);
+            return extract(B)-extract(A);
+        });
         return {
             users: (users??[]).map((user)=>user.toObject())
         }
