277637378b42dd568250e4ebd0d268185e67ed05
qedemon <qedemon@snu.ac.kr>
2024-03-06 16:01:05 +0000

    profile 정보

--- Utility/convertRemoteToLocal.js
+++ Utility/convertRemoteToLocal.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 function convertUser(user){
     return {
         ...user,
         _id: user.user_id,
         name: user.username,
         aaaNo: user.aaa_no,
-        colNo: user.col_no
+        colNo: user.col_no,
+        profilePath: user.profile_path?`${process.env.REMOTE_API_HOST}/static${user.profile_path}`:null
     }
 }
 
