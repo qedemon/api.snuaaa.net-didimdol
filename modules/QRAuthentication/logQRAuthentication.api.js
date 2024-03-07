768b762fca4235c6e5f8adfdfef3bb0e38cf168d
qedemon <qedemon@snu.ac.kr>
2024-03-07 05:42:43 +0000

    sync qrAuthentication

--- modules/QRAuthentication/logQRAuthentication.api.js
+++ modules/QRAuthentication/logQRAuthentication.api.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const {logQRAuthentication} = require("./core");
 const authorize = require("modules/authorize/middleware");
-const {Result} = require("Utility");
+const {Result, Log} = require("Utility");
+const googleAuthorize = require("modules/googleSheet/midlayer");
+const {loadAllQRAuthentications} = require("modules/googleSheet/core");
 
 function attachLogQRAuthentication(app){
     app.use("/logQRAuthentication", authorize);
+    app.use("/logQRAuthentication", googleAuthorize)
     app.use("/logQRAuthentication", express.json());
     app.post("/logQRAuthentication", async (req, res)=>{
         try{
@@ -32,6 +35,15 @@ function attachLogQRAuthentication(app){
                     error: error.message
                 }
             )
+        }
+        try{
+            const {url, error} = await loadAllQRAuthentications(process.env.QR_LOG_GOOGLE_SHEET_ID, req.googleAuthorization?.sheet);
+            if(error){
+                throw error;
+            }
+        }
+        catch(error){
+            Log(error);
         }
     });
     return app;
