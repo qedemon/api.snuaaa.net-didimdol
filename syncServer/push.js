daa3be5c79605bef9865fb9b47736e14862a29ff
qedemon <qedemon@snu.ac.kr>
2024-03-31 21:09:23 +0000

    코드 정리

--- syncServer/push.js
+++ syncServer/push.js
@@ -1,37 +1,41 @@
 require("dotenv").config();
 const {googleAuthorize, loadQRAuthentications} = require("modules/googleSheet/core");
 
+async function updateGoogleSheetForAuthenticationLog(qrAuthentication){
+    try{
+        const {sheet, error} = await googleAuthorize();
+        if(error){
+            throw error;
+        }
+        if(["별모임", "소관", "자율돔관", "etc"].includes(qrAuthentication.type)){
+            return await loadQRAuthentications(process.env.QR_LOG_GOOGLE_SHEET_ID, sheet, 
+                {
+                    "context.title": qrAuthentication.context.title, 
+                    type:{
+                        $in:["별모임", "소관", "자율돔관", "etc"]
+                    }
+                }
+            )
+        }
+        if("디딤돌" === qrAuthentication.type){
+            return await loadQRAuthentications(process.env.QR_DIDIMDOL_LOG_GOOGLE_SHEET_ID, sheet, 
+                {
+                    "context.title": qrAuthentication.context.title, 
+                    type: "디딤돌"
+                }
+            )
+        }
+        return {};
+    }
+    catch(error){
+        return error;
+    }
+}
+
 const pushProcesses = {
     "logQRAuthentication": {
         f: async ({qrAuthentication})=>{
-            try{
-                const {sheet, error} = await googleAuthorize();
-                if(error){
-                    throw error;
-                }
-                if(["별모임", "소관", "자율돔관", "etc"].includes(qrAuthentication.type)){
-                    return await loadQRAuthentications(process.env.QR_LOG_GOOGLE_SHEET_ID, sheet, 
-                        {
-                            "context.title": qrAuthentication.context.title, 
-                            type:{
-                                $in:["별모임", "소관", "자율돔관", "etc"]
-                            }
-                        }
-                    )
-                }
-                if("디딤돌" === qrAuthentication.type){
-                    return await loadQRAuthentications(process.env.QR_DIDIMDOL_LOG_GOOGLE_SHEET_ID, sheet, 
-                        {
-                            "context.title": qrAuthentication.context.title, 
-                            type: "디딤돌"
-                        }
-                    )
-                }
-                return {};
-            }
-            catch(error){
-                return error;
-            }
+            await updateGoogleSheetForAuthenticationLog(qrAuthentication);
         }
     }
 }
