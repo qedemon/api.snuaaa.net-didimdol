9fb62b82cee4acd713bbf53264205faa1c5b281f
qedemon <qedemon@snu.ac.kr>
2024-04-02 21:35:46 +0000

    password hide

--- modules/user/whoAmI.api.js
+++ modules/user/whoAmI.api.js
@@ -3,7 +3,24 @@ const authorize = require("modules/authorize/middleware");
 function attachWhoAmI(app){
     app.use("/whoAmI", authorize);
     app.get("/whoAmI", (req, res)=>{
-        res.status(req?.authorization?.authorized?200:401).json(req.authorization)
+        res.status(req?.authorization?.authorized?200:401).json(
+            (
+                (authorization)=>{
+                    const {userInfo} = authorization;
+                    return userInfo?
+                        {
+                            ...authorization,
+                            userInfo: (
+                                (userInfo)=>{
+                                    const {password, ...remain} = userInfo;
+                                    return remain;
+                                }
+                            )
+                        }:
+                        authorization
+                }
+            )(req.authorization)
+        )
     })
     return app;
 }
